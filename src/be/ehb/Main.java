2a82f1aa8d48132a2738186c5babc7419854d3dd
david.van.steertegem@gmail.com <PowerUser420>
2021-04-21 15:02:32 +0200

    added algorithm for needed output

--- src/be/ehb/Main.java
+++ src/be/ehb/Main.java
@@ -7,7 +7,9 @@ public class Main {
         //vier kleuren in git
         //rood -> niet in versiebeheer
         //groen -> in versiebeheer, nog niet gecommit
-        //
+        //blauw -> in versiebeheer, laatste updates nog niet gecommit
         //zwart ->in versiebeheer, gecommit in de laatste versie
+
+        System.out.println("Hello world");
     }
 }
