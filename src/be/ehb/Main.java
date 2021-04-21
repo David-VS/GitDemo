63702348997ae039f1ac9810dce0ae819a720bd7
david.van.steertegem@gmail.com <PowerUser420>
2021-04-21 14:59:08 +0200

    initial commit of main class

---
+++ src/be/ehb/Main.java
@@ -0,0 +1,13 @@
+package be.ehb;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //vier kleuren in git
+        //rood -> niet in versiebeheer
+        //groen -> in versiebeheer, nog niet gecommit
+        //
+        //zwart ->in versiebeheer, gecommit in de laatste versie
+    }
+}
